b957182584638e440281c635b1373c3365844375
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-01-05 17:25:26 +0000

    feat: connect postgres database

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log("check password", process.env.DB_PASSWORD);
+
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+pool.on("connect", () => {
+    console.log(`Connection pool established with Database...`)
+});
+
+export default pool;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import pool from './config/db.js';
+
 dotenv.config();
+
 
 const app = express();
 
 const port = process.env.PORT || 3000
 
-//middlewares
+// middlewares
 app.use(express.json());
 app.use(cors());
 
-//server running
+// routes 
+app.get("/", async (req, res) => {
+    const result = await pool.query("SELECT current_database()");
+    res.send(`The database name is : ${result.rows[0].current_database}`);
+});
+
+// server running
 app.listen(port, () => {
     console.log(`Server is running on http:localhost:${port}`);
 })
