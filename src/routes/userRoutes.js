2d67aebc35140276319bf5d7e85de105bba508aa
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-01-06 16:28:03 +0000

    feat: add input validations

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controller/userController.js';
+import validateUser from '../middleware/inputValidator.js';
 const router = express.Router();
 
-router.post("/user",createUser);
-router.get("/user",getAllUsers);
-router.get("/user/:id",getUserById);
-router.put("/user/:id",updateUser);
+router.post("/user", validateUser, createUser);
+router.get("/user", getAllUsers);
+router.get("/user/:id", getUserById);
+router.put("/user/:id", validateUser, updateUser);
 router.delete("/user/:id", deleteUser);
 
 export default router;
